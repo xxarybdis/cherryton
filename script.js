35dca917610364e4032a24189e4760c84925c341
xxarybdis <tomarybdis@gmail.com>
2026-09-09 05:51:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ const TOKEN_SLOT_RADIUS_Y = 0.045;
    ----------------------------------------- */
 
 const DISPENSER_X = 62;
-const DISPENSER_Y = 20;
+const DISPENSER_Y = 70;
 
 const DISPENSED_CAPSULE_WIDTH = 10;
 
